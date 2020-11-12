af092f76058b6176d1cf8d1780623abf1b8d2b96
Tamas Papik <trapacska@gmail.com>
2020-11-12 07:03:28 +0100

    added then

--- step.sh
+++ step.sh
@@ -48,7 +48,7 @@ EOF
 
     echo "Check status"
     sleep 5
-    if ! ifconfig | grep tun0 > /dev/null
+    if ! ifconfig | grep tun0 > /dev/null ; then
       echo "No open VPN tunnel found"
       cat "$log_path"
       exit 1
